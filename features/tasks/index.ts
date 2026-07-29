43a0e9cc06f02bb5f4e7bf1a5161c3451c601e52
Sven Sonnborn <sven.sonnborn@skriptfabrik.com>
2026-07-29 12:34:12 +0200

    feat(tasks): Mutations mit optimistischem Cache-Update

--- features/tasks/index.ts
+++ features/tasks/index.ts
@@ -1,7 +1,21 @@
+export { mapTaskError, MissingParentError, type TaskErrorKey } from "./errors";
+export {
+  useCreateTask,
+  useDeleteTask,
+  useToggleTaskDone,
+  useUpdateTask,
+  type CreateTaskVars,
+  type DeleteTaskVars,
+  type ToggleTaskDoneVars,
+  type UpdateTaskVars,
+} from "./mutations";
+export { applyDelete, applyToggle, applyUpdate, type TaskChanges } from "./optimistic";
 export {
   fetchFamilyTasks,
+  fetchTaskTypes,
   taskKeys,
   useFamilyTasks,
+  useTaskTypes,
   useTasksByChild,
   useTasksStats,
 } from "./queries";
